913400341b3e40c61df266dbdbcfcca2cb96ec3e
Egor Aleksandrovich <egor09662@gmail.com>
2022-12-27 18:02:50 +0100

    add new place

--- MyPlaces/PlaceModel.swift
+++ MyPlaces/PlaceModel.swift
@@ -5,14 +5,15 @@
 //  Created by egor aleksandrovich on 26.12.2022.
 //
 
-import Foundation
+import UIKit
 
 struct Place {
     
     var name: String
-    var location: String
-    var type: String
-    var image: String
+    var location: String?
+    var type: String?
+    var restorantImage: String?
+    var image: UIImage?
     
     
     static let restaurantNames = ["Burger Heroes", "Kitchen", "Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"]
@@ -22,7 +23,7 @@ struct Place {
         var places = [Place]()
         
         for place in restaurantNames {
-            places.append(Place(name: place, location: "MInsk", type: "Ресторан", image: place))
+            places.append(Place(name: place, location: "MInsk", type: "Ресторан", restorantImage: place, image: nil))
         }
         
         return places
